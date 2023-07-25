3efc8d1f8375ad5518a4dd6632025e0a6e95d57d
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2023-07-24 22:36:25 -0300

    "comentário"

---
+++ LerXML.py
@@ -0,0 +1,4 @@
+"""Para ler um código de barras de um arquivo XML em Python usando um leitor de código de barras,
+ Podemos usar a biblioteca xml.etree.ElementTree
+para analisar o XML e a biblioteca keyboard para detectar a entrada do teclado do leitor.
+Aqui está um exemplo de código que mostra como fazer isso"""

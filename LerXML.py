7ca60eb8c959d246020a0183f71746061fee0c20
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2023-07-25 20:06:44 -0300

    "import os"

--- LerXML.py
+++ LerXML.py
@@ -2,3 +2,6 @@
  Podemos usar a biblioteca xml.etree.ElementTree
 para analisar o XML e a biblioteca keyboard para detectar a entrada do teclado do leitor.
 Aqui está um exemplo de código que mostra como fazer isso"""
+
+import xml.etree.ElementTree as ET
+import os

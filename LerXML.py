9115f8c8aae412c2a800d78c129aa1fc8305b161
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2023-08-01 22:43:28 -0300

    "Return"

--- LerXML.py
+++ LerXML.py
@@ -14,5 +14,6 @@ def ler_codigo_de_barras(xml_file):
 # Encontre o elemento que contém o código de barras
     codigo_de_barras = root.find('CodigoDeBarras').text
 
-
 # Validação do código de barras
+
+    return codigo_de_barras

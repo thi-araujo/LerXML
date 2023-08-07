ab35380c6e55cc14f6ff340b91393573ed20066f
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2023-08-07 20:06:58 -0300

    "for arquivo"

--- LerXML.py
+++ LerXML.py
@@ -21,6 +21,6 @@ def ler_codigo_de_barras(xml_file):
 def validar_codigos_de_barras_em_pasta(pasta):
     arquivos_xml = [f for f in os.listdir(pasta) if f.endswith('.xml')]
 
-    # Faça a validação do código de barras aqui
+    for arquivo_xml in arquivos_xml:
 
-print(f'Arquivo: {arquivo_xml}, Código de Barras: {codigo_de_barras}, Validação: {validacao}')
+
